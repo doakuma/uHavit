f87be99795708e7feefa34876f42dc4c01507824
doakuma <doakuma@gmail.com>
2020-02-04 21:26:42 +0900

    uSetting

--- uhavit/src/view/uSetting/uSetting.js
+++ uhavit/src/view/uSetting/uSetting.js
@@ -1,21 +1,74 @@
-import React, { Component }from 'react';
+import React, { useState }from 'react';
 import Container from '../../container/Container';
 import Title from '../../components/Title';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
-class USetting extends Component {
-    render() {
-        return(
-            <Container>
-                <Title level={2} text="uSetting"/>
 
-                <form>
-                    Input
-                </form>
-            </Container>
-        )
+const USetting = () => {
+    const [setInfo, setState] = useState({
+        uWant: '',
+        uFreq: '',
+        uWhen: ''
+    })
+
+    const updateField = e => {
+        setState({
+            ...setInfo,
+            [e.target.name]: e.target.value
+        })
     }
+
+    const resetLogin = (e) => {
+        setState({
+            [e.target.name]: ''
+        })
+    }
+
+    const confirmLogin = (e) => {
+        e.preventDefault();
+        console.log(setInfo)
+    }
+    return(
+        <Container>
+            <Title level={2} text="uSetting"/>
+
+            <form>
+                <Input
+                    title="What U want"
+                    id="uWant"
+                    name="uWant"
+                    type="text"
+                    value={setInfo.uWant}
+                    handleChang={updateField}
+                    placeholder="Write What U want"
+                />
+                <Input
+                    title="How U often"
+                    id="uFreq"
+                    name="uFreq"
+                    type="number"
+                    value={setInfo.uFreq}
+                    handleChang={updateField}
+                    placeholder="Write How U want"
+                />
+                <Input
+                    title="When U want"
+                    id="uWhen"
+                    name="uWhen"
+                    type="number"
+                    value={setInfo.uWhen}
+                    handleChang={updateField}
+                    placeholder="Write When U want"
+                />
+                <Button 
+                    type="submit"
+                    title="SetUp"
+                />
+            </form>
+        </Container>
+    )
+
 }
 
 export default USetting;
