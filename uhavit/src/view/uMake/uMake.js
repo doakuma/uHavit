5f366c25442e650bbd768742482bcb4f577b4dba
doakuma <doakuma@gmail.com>
2020-02-04 23:33:42 +0900

    uMake

--- uhavit/src/view/uMake/uMake.js
+++ uhavit/src/view/uMake/uMake.js
@@ -1,14 +1,39 @@
-import React, { Component }from 'react';
+import React, { useState }from 'react';
 import Container from '../../container/Container';
+import Title from '../../components/Title';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
-class UMake extends Component {
-    render() {
-        return(
-            <Container>
-            UMake
-            </Container>
-        )
+/**
+ * TODO :
+ * 
+ * 
+ */
+
+const UMake = () => {
+    const startHavit = e => {
+        e.preventDefault();
+        console.log('start havit')
     }
+    const writeToday = e => {
+        e.preventDefault();
+        console.log('write today')
+    }
+    return(
+        <Container>
+            <Title level={2} text="UMake"/>
+            <Button
+                type="button"
+                title="Start Havit"
+                action={startHavit}
+            />
+            <Button
+                type="button"
+                title="Write Today"
+                action={writeToday}
+            />
+        </Container>
+    )
 }
 
 export default UMake;
